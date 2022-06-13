ade3d5062376ef94ce228c7e216414be28591184
Isaac Poole <isaac@switcherstudio.com>
2022-06-13 08:44:17 -0400

    uses text for version

--- src/helpers/version.py
+++ src/helpers/version.py
@@ -1,10 +1,8 @@
 import os
-from json import load
 
 
 def get_version():
     f = open(os.path.join(
         '/'.join(os.path.realpath(__file__).split('/')[:-2]), 'data.json'))
-    data = load(f)
 
-    return data['version']
+    return f.read()
