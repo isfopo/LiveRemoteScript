082e631a5f376a48495f6b2ea36387d1e119b315
Isaac Poole <isaac@switcherstudio.com>
2022-06-13 21:09:00 -0400

    finds version file

--- src/helpers/version.py
+++ src/helpers/version.py
@@ -3,6 +3,6 @@ import os
 
 def get_version():
     f = open(os.path.join(
-        '/'.join(os.path.realpath(__file__).split('/')[:-2]), 'data.json'))
+        '/'.join(os.path.realpath(__file__).split('/')[:-2]), 'version.txt'))
 
     return f.read()
